8c9e32fd42fdec3c49e7cd0b448833f3a98506b0
Aarthi212 <70945036+Aarthi212@users.noreply.github.com>
2021-01-07 19:13:32 +0530

    Update Search.c

--- Search.c
+++ Search.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 int main()
 {
-	int n,i,pos=-1,*a,element;
-	scanf("%d",&n);
-	a=calloc(sizeof(int),n);
-	for(i=0;i<n;i++)
+  int n,i,pos=-1,*a,element;
+  scanf("%d",&n);
+  a=calloc(sizeof(int),n);
+  for(i=0;i<n;i++)
+   {
+     scanf("%d",a+i);
+   }
+     scanf("%d",&element);
+     for(i=0;i<n;i++)
+      {
+	if(*(a+i)==element)
 	{
-		scanf("%d",a+i);
+	  pos=i;
+	  break;
 	}
-	scanf("%d",&element);
-	for(i=0;i<n;i++)
-	{
-		if(*(a+i)==element)
-		{
-			pos=i;
-			break;
-		}
-	}
-	printf("%d is in %d",element,pos);
-	return 0;
+      }
+    printf("%d is in %d",element,pos);
+    return 0;
 }
