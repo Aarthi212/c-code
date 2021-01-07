6d88558044c881b600760b1b4d40586ae12bb1f3
Aarthi212 <70945036+Aarthi212@users.noreply.github.com>
2021-01-07 19:19:21 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,19 +1,17 @@
 #include<stdio.h> 
 int main() 
 { 
-	int i, j, k;
-	char str[100];
-	char rev[100];
-	scanf("%s", str);
-	  for(i = 0; str[i] != '\0'; i++);
-	    {
-		    k = i-1;
-    	}
-  	
-		for(j = 0;j <= i-1; j++)
-			{
-				rev[j] = str[k]; k--;
-			}
-			
-			printf("%s\n", rev);
+ int i, j, k;
+ char str[100];
+ char rev[100];
+ scanf("%s", str);
+ for(i = 0; str[i] != '\0'; i++);
+  {
+    k = i-1;
+  }
+  for(j = 0;j <= i-1; j++)
+  {
+    rev[j] = str[k]; k--;
+  }
+ printf("%s\n", rev);
 }
