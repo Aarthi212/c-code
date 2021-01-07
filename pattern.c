6d78324390256d261d6966b4d47aaa70f97e85d2
Aarthi212 <70945036+Aarthi212@users.noreply.github.com>
2021-01-07 19:16:31 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -1,22 +1,22 @@
 #include <stdio.h> 
 int main() 
 {
-	int i, j, rows;
-	 scanf("%d", &rows);
-	 for (i = 0; i < rows; i++)
-	 {
-	 	for (j = 0; j < rows; j++)
-	 	{
-	 		if (j < i)
-	 		{
-	 			printf(" ");
-	 		}
-	 			else
-	 			{
-	 				printf("*");
-	 			}
-	 	}
-	 				printf("\n");
-	 }
-	 				return 0;
+ int i, j, rows;
+ scanf("%d", &rows);
+ for (i = 0; i < rows; i++)
+ {
+  for (j = 0; j < rows; j++)
+  {
+    if (j < i)
+     {
+	printf(" ");
+     }
+    else
+     {
+        printf("*");
+     }
+  }
+  printf("\n");
+ }
+ return 0;
 }
